d8a8b182930595fc5f0fbe53d1e452556ff285a3
Lucas Botelho <lucasbotelho97@hotmail.com>
2025-05-17 01:18:35 -0300

    insering clubs shields

--- src/main.py
+++ src/main.py
@@ -35,15 +35,6 @@ def save_data(partidas, jogadores):
     partidas.to_csv(FILE_PARTIDAS, index=False)
     jogadores.to_csv(FILE_JOGADORES, index=False)
 
-
-def carregar_imagem_url_fundo_branco(url):
-    response = requests.get(url)
-    imagem_transparente = Image.open(BytesIO(response.content)).convert("RGBA")
-    fundo_branco = Image.new("RGBA", imagem_transparente.size, (255, 255, 255, 255))
-    imagem_com_fundo = Image.alpha_composite(fundo_branco, imagem_transparente)
-    imagem_rgb = imagem_com_fundo.convert("RGB")
-    return imagem_rgb
-
 # Tela Principal com gráficos simples e indicadores
 def tela_principal(partidas, jogadores):
     st.title("Chopp's League")
@@ -53,11 +44,11 @@ def tela_principal(partidas, jogadores):
     col1, col2 = st.columns(2)
 
     with col1:
-        image = carregar_imagem_url_fundo_branco("https://static.vecteezy.com/ti/vetor-gratis/t1/27727704-borussia-dortmund-clube-logotipo-simbolo-futebol-bundesliga-alemanha-abstrato-projeto-ilustracao-com-preto-fundo-gratis-vetor.jpg")
+        image = Image.open("./imagens/borrusia_escudo.jpg")
         st.image(image, caption="Borrusia",  use_container_width =True)
 
     with col2:
-        image = carregar_imagem_url_fundo_branco("https://static.vecteezy.com/ti/vetor-gratis/p1/27464867-inter-milao-clube-logotipo-simbolo-serie-uma-futebol-calcio-italia-abstrato-projeto-ilustracao-com-preto-fundo-gratis-vetor.jpg")
+        image = Image.open("./imagens/inter_escudo.jpg")
         st.image(image, caption="Inter",  use_container_width =True)
 
     st.header("Resumo das Partidas")
